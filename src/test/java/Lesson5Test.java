fc9733515e90c546a4e53f5b85329b58b1bc5294
Alina K <alina885@mail.ru>
2025-03-11 23:48:01 +0300

    added Lesson5Test

--- src/test/java/Lesson5Test.java
+++ src/test/java/Lesson5Test.java
@@ -1,7 +1,12 @@
+
+import com.codeborne.selenide.DragAndDropOptions;
+import com.codeborne.selenide.Selenide;
 import com.codeborne.selenide.Configuration;
+import com.codeborne.selenide.SelenideElement;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
+import static com.codeborne.selenide.Condition.exactText;
 import static com.codeborne.selenide.Condition.text;
 import static com.codeborne.selenide.Selectors.byText;
 import static com.codeborne.selenide.Selenide.*;
@@ -21,5 +26,26 @@ public class Lesson5Test {
         $$(".HeaderMenu-dropdown-link").findBy(text("Enterprises")).click();
         $("#hero-section-brand-heading").shouldHave(text("The AI-powered developer platform"));
 
+    }
+
+    @Test
+    void testDragAndDropWithActions() {
+        open("https://the-internet.herokuapp.com/drag_and_drop");
+        SelenideElement elementA = $("#column-a");
+        SelenideElement elementB = $("#column-b");
+        actions().clickAndHold(elementA).moveToElement(elementB).release().perform();
+        elementA.shouldHave(text("B"));
+        elementB.shouldHave(text("A"));
+    }
+
+    @Test
+    void testDragAndDrop() {
+
+        open("https://the-internet.herokuapp.com/drag_and_drop");
+        SelenideElement elementA = $("#column-a");
+        SelenideElement elementB = $("#column-b");
+        elementA.dragAndDrop(DragAndDropOptions.to(elementB));
+        elementA.shouldHave(text("B"));
+        elementB.shouldHave(text("A"));
     }
 }
